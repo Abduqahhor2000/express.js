06d98319e67f9bde79e190aeedae55ac985c8131
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2021-12-24 12:34:11 +0500

    express start

---
+++ Lesson/get.js
@@ -0,0 +1,15 @@
+// const express = require("express")
+// const app  = express()
+// const path = require("path")
+
+// app.listen(5000, () => {console.log("server qchu ishlovotti")})
+
+// app.get("/", (req, res, next)=>{
+//     // res.status(500).send("hello year")
+//     // res.download("index.js")
+//     res.sendFile(path.join(__dirname, "views", "index.html"))
+// })
+
+// app.get("/node", (req, res) => {
+//     res.send("hello bratishka")
+// })
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app  = express()
+app.set("view engine", "ejs")
 
 app.listen(5000, () => {console.log("server qchu ishlovotti")})
 
-app.get("/", ()=>{console.log("kaka")})
+app.get("/", (req, res, next)=>{
+    res.render("index", {title: "Abduqahhor"})
+})
+
